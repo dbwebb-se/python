bcb94e8747f5dba51e51840a06ea84b2a87b5219
Mikael Roos <mikael.t.h.roos@gmail.com>
2014-05-27 17:45:28 +0200

    Adding hello world examples

---
+++ example/hello_world/hw_function.py
@@ -0,0 +1,24 @@
+"""
+A function to say Hello, just to show off how functions and docstrings works.
+
+Use it like this.
+
+>>> import hw_function as hw
+>>> help(hw)
+>>> hw.say_it.__doc__
+>>> hw.__doc__
+
+Also try pydoc like this in the terminal:
+
+pydoc sys
+
+"""
+
+def say_it():
+    """
+    Just sayin to the world that we are here and now know python.
+
+    Ths function is currently defined in a module named __name__.
+    """
+    print("\nHello World in module %s\n" % __name__)
+
---
+++ example/hello_world/hw_main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""
+Calling the function what says hello from a main-method and using docstrings to
+document it all. Showing how to import a file (module) with an external function.
+
+Import it and run it by calling the main function or run it as a script.
+
+1) In the python interpreter
+
+>>> import hw_main as hw
+>>> hw.main()
+
+
+2) In the shell
+
+$ python3 hw_main.py
+
+
+3) Make it executable and just run it.
+
+$ chmod 755 hw_main.py
+$ ./hw_main.py
+
+"""
+
+import hw_function as hw
+
+
+def main():
+    """
+    This is the main method, I call it main by convention.
+    """
+    print(hw.say_it.__doc__)
+    hw.say_it()
+
+
+
+if __name__ == "__main__":
+        print(main.__doc__)
+        main()
