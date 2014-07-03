0a2356e74f883ad73ae1793ad59e31f6d8e2e971
Mikael Roos <mikael.t.h.roos@gmail.com>
2014-07-03 13:03:04 +0200

    adding hello for intro article!

---
+++ example/hello_world/hello.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Some various ways of saying Hello World in Python
+"""
+
+def hello():
+    """
+    Print out Hello World in a function.
+    """
+    print("Hello World in a function.")
+
+# Call a function that prints out Hello World
+hello()
+
+# Print out Hello World 
+print("Just saying Hello World")
+
+# Assign the string Hello World to a variable and print it out
+str = "Hello World in a variable"
+print(str)
