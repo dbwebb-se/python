d3718a6714cb4b21288db52f8ea67c473aa67849
reechani <j.reechani@gmail.com>
2014-08-27 09:20:40 +0200

    Fixed some jslint

--- example/scrape/tv-tabla.py
+++ example/scrape/tv-tabla.py
@@ -14,9 +14,9 @@ import sys
 # http://stackoverflow.com/a/6594775
 try:
     import urllib.request as urllib2
-except:
+except ImportError:
     import urllib2
-# if you're using BeautifulSoup4:
+# pip install beautifulsoup4
 from bs4 import BeautifulSoup
 
 # argument from command line
@@ -28,17 +28,21 @@ else:
 # Link
 TV = "http://www.tv.nu/"
 
-# soup
+# Get the webpage content as a soup
 soup = BeautifulSoup(urllib2.urlopen(TV))
 
 def getTabla(theSoup):
     """
     Uses the soup to filter out current shows on TV
     """
+    # Get the container for each channel
     for row in theSoup("div", {"class": "tabla_container"}):
+        # Get the title of each channel
         for title in row("p", {"class": "tabla_topic_label"}):
+            # Continue if the title is a match to input or everything ("")
             if CHANNEL.lower() in title.text.lower():
                 print(title.text.strip())
+                # Get the current program
                 for current in row("li", {"class": "po"}):
                     print("\t", current.text.strip(), "-", current.a.text.strip())
                 print("-----")
