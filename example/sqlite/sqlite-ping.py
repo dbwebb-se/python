29a0fa499b10cd59532d6bb45ae472c56c94bae7
reechani <j.reechani@gmail.com>
2014-08-27 09:15:10 +0200

    added comments to examples and sqlite-example no creates table if there isn't one

--- example/sqlite/sqlite-ping.py
+++ example/sqlite/sqlite-ping.py
@@ -1,42 +1,54 @@
 #!/usr/bin/env python3
 """
 Test example to take a ping and save it into an sqlite3 database
+https://docs.python.org/3/library/sqlite3.html
 """
-
+# pip install requests
 import requests
-import time
 import sqlite3
+import time
 import sys
 
+# Get argument if there is one and use that as url
 if len(sys.argv) == 2:
     url = sys.argv[1]
 else:
     url = "http://dbwebb.se/"
 
+# connect to the database and set a cursor
 conn = sqlite3.connect("ping.sqlite3")
 cur = conn.cursor()
 
-# init
-# cur.execute("DROP TABLE IF EXISTS Pings")
-# cur.execute("CREATE TABLE Pings (url TEXT, time TEXT PRIMARY KEY, repsonse INTEGER)")
+# init database incase it's lost
+cur.execute("""
+CREATE TABLE IF NOT EXISTS Pings
+(url TEXT, time TEXT PRIMARY KEY, repsonse INTEGER)""")
 
+# get current time for logging
 rTime = time.ctime(time.time())
 
 try:
-    # url must start with "http://"
+    # url must start with "http://" or the request fails
     r = requests.head(url)
     code = r.status_code
 except requests.ConnectionError:
     code = 0
 
+# create the values to be added as tuple
 values = (url, rTime, code)
-cur.execute("INSERT INTO Pings (url, time, repsonse) VALUES (?, ?, ?)", values)
+# insert values into database
+cur.execute("""
+INSERT INTO Pings (url, time, repsonse)
+VALUES (?, ?, ?)
+""", values)
 
 print("Rows in db:")
+# get all the rows in database
 cur.execute("SELECT url, time, repsonse FROM Pings")
+# loop through the result
 for row in cur:
     print(row)
 
-
+# commit the changes and close the connection
 conn.commit()
 conn.close()
