bb1c67564f9fb4d7477efb97be89521815468945
Mikael Roos <mikael.t.h.roos@gmail.com>
2014-09-17 13:53:21 +0200

    changed loads to load, dumps to dump

--- example/json/parse-json.py
+++ example/json/parse-json.py
@@ -1,39 +1,41 @@
 #!/usr/bin/env python3
 """
-Testing out reading a writing json to a file
+Testing out reading and writing json to a file
 """
 
 import json
 
+
 # Open file and read all its contents
-fhand = open("json.txt", "r")
-content = fhand.read()
-fhand.close()
+jsonfile = open("json.txt", "r")
 
 # Decode json
-stuff = json.loads(content)
+jsonobject = json.load(jsonfile)
+
 
 # Print out the content from the dict
-for color in stuff["colorsArray"]:
+for color in jsonobject["colorsArray"]:
     print(color["colorName"], color["hexValue"])
+
 
 # Ask if color is to be added
 confirm = input("Do you want to add a color (y/n)? ")
 
+
 if confirm.lower() == "y":
+
     # get new color
     colorName = input("Enter color name: ")
     hexValue = input("Enter hex value (with #): ")
 
     # append the color as a dict to the colorsarray
-    stuff["colorsArray"].append({
+    jsonobject["colorsArray"].append({
         "colorName": colorName,
         "hexValue": hexValue
     })
 
     # Open the file for writing ("w" will replace the file contents)
-    fhand = open("json.txt", "w")
-    # Encode json with pretty output (indent)
-    fhand.write(json.dumps(stuff, indent=4))
-    fhand.close()
+    jsonfile = open("json.txt", "w")
 
+    # Encode json with pretty output (indent)
+    json.dump(jsonobject, jsonfile, indent=4)
