a4d037ee1a13fa6f2133bd15d8feb4140704b78e
Mikael Roos <mikael.t.h.roos@gmail.com>
2014-10-13 18:07:01 +0200

    adding encoding utility

---
+++ example/scrape/encoding/encoding.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import locale
+import os
+
+print("Default encoding is (sys.getdefaultencoding()): ", sys.getdefaultencoding())
+print("(sys.stdin.encoding):                           ", sys.stdin.encoding)
+print("(sys.stdout.encoding):                          ", sys.stdout.encoding)
+print("(sys.stderr.encoding):                          ", sys.stderr.encoding)
+
+print("Default locale is (locale.getdefaultlocale()):  ", locale.getdefaultlocale())
+
+print("Environment variable PYTHONIOENCODING: ", os.environ.get("PYTHONIOENCODING"))
+print("Environment variable LANG:             ", os.environ.get("LANG")) 
+print("Environment variable LC_CTYPE:         ", os.environ.get("LC_CTYPE"))
+
+print("\u03b1\u03b2\u03b3")
+print("░")
+
