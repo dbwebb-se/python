74d5f3b7a14d8058c0546e5a7b0b98ebec267bcf
Mikael Roos <mikael.t.h.roos@gmail.com>
2015-03-13 17:27:43 +0100

    simpler code

--- example/file/list-to-file.py
+++ example/file/list-to-file.py
@@ -3,59 +3,34 @@
 Example of how to read the content of a file into a list
 """
 
-def read_file(filename):
-    """
-    Read a single line file and split it to a list
-    """
-    # with - as for reading a file automatically closes it after reading is done
-    with open(filename) as filehandle:
-        line = filehandle.readline();
+# the name of the file
+filename = "items.txt"
 
-    # print the line read from the file
-    print(line)
-
-    # split the line into a list on the comma ","
-    items_as_list = line.split(",")
-
-    # print what the list looks like
-    print(items_as_list)
-
-    return items_as_list
+# with - as for reading a file automatically closes it after reading is done
+with open(filename) as filehandle:
+    line = filehandle.readline();
+# print the line read from the file
+print(line)
 
 
-def save_list(list, filename):
-    """
-    Save a list of items to file, comma-separated
-    """
-    # join the list into a string with a comma ","" between every item
-    list_as_str = ",".join(list)
-
-    # show what the string looks like after join
-    print(list_as_str)
-
-    # write the line to the file
-    with open(filename, "w") as filehandle:
-        filehandle.write(list_as_str)
+# split the line into a list on the comma ","
+items_as_list = line.split(",")
+# print what the list looks like
+print(items_as_list)
 
 
-def main():
-    """
-    Main function to carry out the work.
-    """
-
-    # read the file into list
-    items = read_file("items.txt")
-
-    # add item to the list
-    items.append("cup")
-
-    # print what the list looks like after change
-    print(items)
-
-    # save the file
-    save_list(items, "items.txt")
+# add item to the list
+items_as_list.append("cup")
+# print what the list looks like after change
+print(items_as_list)
 
 
-if __name__ == "__main__":
-    print(__doc__)
-    main()
+# join the list into a string with a comma ","" between every item
+list_as_str = ",".join(items_as_list)
+# show what the string looks like after join
+print(list_as_str)
+
+
+# write the line to the file
+with open(filename, "w") as filehandle:
+    filehandle.write(list_as_str)
