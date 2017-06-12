9412d9bd93251089d4d2e12b7f2f5eff715d480c
Jakob Sandgren <sandgren.jakob@gmail.com>
2017-06-12 12:20:24 +0200

    Fixed style to make the messages.md easier to read.

--- example/pylint/generate.bash
+++ example/pylint/generate.bash
@@ -3,14 +3,15 @@
 # Read pylint error codes to md-file.
 
 echo "# Pylint error codes" > messages.md
-echo "This file is based on Pylint version 1.6.5" >> messages.md
+echo 'This is the messages given from the command `pylint --list-msgs` with Pylint version 1.6.5' >> messages.md
 pylint --list-msgs | while read line ;
+
 do
     if [[ $line == :* ]];then
+        echo "___"
         printf "## "
         echo "$line"|awk '{print $1}'
-        echo "---"
-        printf "#### "
+        printf "##### "
         echo "$line"|awk '{$1=""; print $0}'
     else
         echo $line
