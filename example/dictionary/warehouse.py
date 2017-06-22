2c98d0c31035fd01186323ee81708036933f9bcd
Emil Folino <emil.folino@bth.se>
2017-06-22 09:19:02 +0200

    Finished example for dictionary and tuples

--- example/dictionary/warehouse.py
+++ example/dictionary/warehouse.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+Code examples for dictioanries
+"""
+
 warehouse = {
     "köttfärs" : 20,
     "grädde" : 80,
@@ -17,3 +21,37 @@ print(warehouse)
 
 for key, value in warehouse.items():
     print(key, value)
+
+for key in sorted(warehouse.keys()):
+    print(key, warehouse[key])
+
+warehouse_deluxe = {
+    "köttfärs" : { "stock" : 20, "price" : 50 },
+    "grädde" : { "stock" : 80, "price" : 20 },
+    "krossade tomater": { "stock" : 33, "price" : 10 },
+    "gul lök" : { "stock" : 42, "price" : 5 }
+}
+
+for key in sorted(warehouse_deluxe.keys()):
+    print(key, warehouse_deluxe[key]["price"])
+
+warehouse_deluxe = {
+    "köttfärs" : { "stock" : 20, "price" : 50, "ids" : (1234, "K14") },
+    "grädde" : { "stock" : 80, "price" : 20, "ids" : (3141, "L12") },
+    "krossade tomater": { "stock" : 33, "price" : 10, "ids" : (4224, "E13") },
+    "gul lök" : { "stock" : 42, "price" : 5, "ids" : (2742, "D02") }
+}
+
+warehouse_deluxe["röd lök"] = {}
+warehouse_deluxe["röd lök"]["stock"] = 7
+warehouse_deluxe["röd lök"]["price"] = 9
+warehouse_deluxe["röd lök"]["ids"] = (6314, "D04")
+
+for key in sorted(warehouse_deluxe.keys()):
+    print("{product} costs {price} and we have {stock} in stock. It has barcode {barcode} and stock id {stock_id}.".format(
+        product=key,
+        price=warehouse_deluxe[key]["price"],
+        stock=warehouse_deluxe[key]["stock"],
+        barcode=warehouse_deluxe[key]["ids"][0],
+        stock_id=warehouse_deluxe[key]["ids"][1]
+    ))
