894bd453d3363cc7d461cf0328422fae4af39ebb
lewenhagen <lewenhagen@live.com>
2017-06-26 09:31:31 +0200

    added default attrib

--- example/argparse/usage.py
+++ example/argparse/usage.py
@@ -15,15 +15,13 @@ options = {}
 
 def parse_options():
     """
-    Parse all command line options and arguments.
-
-    Return them as a dictionary.
+    Parse all command line options and arguments and return them as a dictionary.
     """
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group()
 
-    group.add_argument("-v", "--verbose", dest="verbose", help="increase output verbosity", action="store_true")
-    group.add_argument("-s", "--silent", dest="silent", help="decrease output verbosity", action="store_true")    
+    group.add_argument("-v", "--verbose", dest="verbose", default="False", help="increase output verbosity", action="store_true")
+    group.add_argument("-s", "--silent", dest="silent", default="False", help="decrease output verbosity", action="store_true")
 
     parser.add_argument("-V", "--version", action="version", version=VERSION)
 
