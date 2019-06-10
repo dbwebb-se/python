f0a1849e8425a33f6eb6133ece1a5735ce78fb7f
Andreas Arnesson <Andreas_osby@hotmail.com>
2019-06-10 10:21:56 +0200

    files for debugger excercise

---
+++ example/debugger/functions.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Example script showing definition and usage of functions in python.
+For the article Funktioner, parametrar och returvärden.
+"""
+
+def calculate_energy_return(time_in_microwave, effect=800):
+    """
+    Calculates the energy consumption i kWh
+    Returns the consumption
+    """
+    energy = effect * time_in_microwave / 1000
+    return energy
+
+def calculate_cost(energy, price_p_kwh=78.04):
+    """
+    Calculates the cost for a given energy consumption
+    Returns the cost in kr
+    """
+    cost = energy * price_p_kwh / 100
+    return cost
+
+emil_time = 2.5 / 60
+emil_energy = calculate_energy_return(emil_time)
+emil_cost = calculate_cost(emil_energy)
+
+print("Emil använder " + str(emil_energy) + " kWh och detta kostar " + str(emil_cost) + " kr")
+
+nice_string = "Emil använder {energy:.4f} kWh och detta kostar {cost:.4f} kr".format(
+    energy=emil_energy,
+    cost=emil_cost
+)
+print(nice_string)
---
+++ example/debugger/while.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Example script showing usage of while-loop for article
+Villkor och loopar
+"""
+
+while True:
+    user_input = input("Skriv in antal äpplen (eller q för avslut): ")
+    if user_input == "q":
+        print("Du är nu klar med att äta äpplen.")
+        print("Hej då!")
+        break
+    else:
+        try:
+            number_of_apples = int(user_input)
+        except ValueError:
+            print("Oj! Du skrev inte in en siffra.")
+            continue
+            
+        if number_of_apples > 10:
+            print("Du har mer än 10 äpplen")
+        elif number_of_apples <= 10 and number_of_apples > 5:
+            print("Du blev snabbt mätt och åt bara upp några av dina äpplen")
+        else:
+            print("Du har nog varit hungrig och ätit upp dina äpplen")
+        
