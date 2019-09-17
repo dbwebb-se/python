10555d5624b156d5517198a303e9ebdce2ce85a1
Andreas Arnesson <andreas_osby@hotmail.com>
2019-09-17 18:30:52 +0200

    example code for kmom02 part 2

---
+++ example/lectures/2019/kmom02/debugger.py
@@ -0,0 +1,16 @@
+"""
+Lets debug!
+"""
+variable = """
+Phil played two or three times more, managing to obtain in all 
+twenty-five cents additional. He reached the corner of Thirteenth Street 
+just as the large public school, known as the Thirteenth Street School, 
+was dismissed for its noon intermission."""
+
+something = """,".:-'?"""
+variable = variable.lower()
+house = 0
+for lobster in variable:
+    if lobster in something:
+        house += 1
+print(house)
