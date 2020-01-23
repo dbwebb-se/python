b64822fe29f550865bb163fc1ee754c9ca2334dd
Andreas Arnesson <andreas_osby@hotmail.com>
2020-01-23 11:32:19 +0100

    spelling

--- example/real_path/real_path.py
+++ example/real_path/real_path.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-How to get absolute path for current firectory
+How to get absolute path for current directory
 """
 import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
