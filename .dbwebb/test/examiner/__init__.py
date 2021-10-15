87736ccc374f9fcb9aff732930599a33ac58e25c
Circle Ci python-examination <circleci@python.examination>
2021-10-15 13:50:09 +0000

    python-examination from CircleCI
    b870053458886894d41e01803d22b0a3076c96fb docs released v2.1.1
    
    [skip ci]

--- .dbwebb/test/examiner/__init__.py
+++ .dbwebb/test/examiner/__init__.py
@@ -4,7 +4,8 @@ Meta data for code
 from examiner.exam_test_case import ExamTestCase
 from examiner.exam_test_case_exam import ExamTestCaseExam
 from examiner.exam_test_result import ExamTestResult
+from examiner.exam_test_result_exam import ExamTestResultExam
 from examiner.helper_functions import check_for_tags as tags, find_path_to_assignment, import_module
 
 # Version structure major.minor[.patch][sub]
-__version__ = '2.1.0'
+__version__ = '2.1.1'
