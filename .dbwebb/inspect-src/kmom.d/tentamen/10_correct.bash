09d32a0c7c602f9b52eebe00ec935229fd4f089e
Andreas Arnesson <andreas_osby@hotmail.com>
2022-01-10 09:26:00 +0100

    run test with no-validate

--- .dbwebb/inspect-src/kmom.d/tentamen/10_correct.bash
+++ .dbwebb/inspect-src/kmom.d/tentamen/10_correct.bash
@@ -4,8 +4,9 @@ e() { exit; }; export -f e
 f() { exit 1; }; export -f f
 
 echo "Run correct script"
-dbwebb test tentamen
+dbwebb test tentamen --no-validate
 
+# cant upload from inside docker so we run manual validate
 cd ../../ && make validate what=me/tentamen
 
 read -p "Press to view code" answer
